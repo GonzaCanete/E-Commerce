497a60314cb92ca8d953956f452e4c2f96bed66e
Gonza Cañete <gonzalocb1997@gmail.com>
2023-05-14 16:34:35 -0300

    login/register fixed

--- js/app.js
+++ js/app.js
@@ -15,82 +15,7 @@
 //     console.log(cartCost.textContent);
 // })
 
-// probando registro
 
-// const registerUser = []
-// const name = document.querySelector("#name");
-// name.addEventListener("input",(e)=>{
-//     console.log(e.target.value);
-// })
-// const lastname = document.querySelector("#lastname");
-// registerUser.push(lastname.value)
-// const email = document.querySelector("#email");
-// registerUser.push(email.value)
-// const password = document.querySelector("#password");
-// registerUser.push(password.value)
-// const confirmPassword = document.querySelector("#confirm-password");
-// registerUser.push(confirmPassword.value)
-// const buttonRegister = document.querySelector("#register-button");
-// buttonRegister.addEventListener("click", (e)=>{
-//         e.preventDefault();
-//         console.log(registerUser);
-//         const registerUserStr = JSON.stringify(registerUser);
-//         localStorage.setItem("registerUser", registerUserStr);
-//     })
-
-// // probando login
-
-// const loginEmail = document.querySelector("#login-email");
-// const loginPassword = document.querySelector("#login-password");
-// const buttonLogin = document.querySelector("#login-button");
-// buttonLogin.addEventListener("click", (e)=>{
-//     const registerUserRecovery = JSON.parse(localStorage.getItem("registerUser"))
-//     e.preventDefault();
-//     if (loginEmail.value === registerUserRecovery.email && loginPassword.value === registerUserRecovery.confirmPassword) {
-//         console.log("Ingreso correcto");
-//     }else{
-//         console.log("Ingreso incorrecto");
-//     }
-//     })
-
-
-// local storage guarda por más tiempor que el session storage
-// para mandar al local storage hay que pasarlo a JSON 
-// para pasar un objeto o array a JSON, tenemos la función stringify
-
-// const playstation5 = {
-//     name: "Playstation 5",
-//     price: 1499,
-// }
-
-// const playstation5str = JSON.stringify(playstation5);
-// localStorage.setItem("playstation 5", playstation5str)
-
-// const array = ["play1", "play2"];
-// const arraystr = JSON.stringify(array);
-// localStorage.setItem("array", arraystr)
-
-// const array = [1, 2, 3, 3, 4, 5, 6, 6];
-// const arraystr = JSON.stringify(array);
-// localStorage.setItem("array", arraystr);
-
-// const nombre = prompt ("Ingrese su nombre");
-// const nombrestr = JSON.stringify(nombre);
-// localStorage.setItem("nombre", nombrestr);
-
-// const nombreRecuperado = JSON.parse(localStorage.getItem("nombre"));
-// console.log(nombreRecuperado);
-
-// const nombres = [];
-// for (i = 1; i <= 3; i++){
-//     const name = prompt ("Ingrese su nombre");
-//     nombres.push(name);
-// }
-// const nombresstr = JSON.stringify(nombres);
-// localStorage.setItem("nombres", nombresstr);
-
-// const namesRecovery = JSON.parse(localStorage.getItem("nombres"))
-// console.log(namesRecovery);
 
 // Otra prueba de registro con localStorage --- FUNCIONA!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // Leo los datos de registro
@@ -101,7 +26,6 @@ const userPassword = document.querySelector("#password");
 const userPassword2 = document.querySelector("#confirm-password");
 const regButton = document.querySelector("#register-button");
 regButton.addEventListener("click", (e)=>{
-    e.preventDefault();
     console.log(userEmail.value);
     // Creo un object con los datos ingresados en registro
     const user = {
@@ -115,22 +39,20 @@ regButton.addEventListener("click", (e)=>{
     localStorage.setItem("user", userStr);
 })
 
-// Prueba de login 
+// Prueba de login  FUNCIONA!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // Leo los datos de login
 const userEmailLogin = document.querySelector("#login-email");
 const userPasswordLogin = document.querySelector("#login-password");
 const loginButton = document.querySelector("#login-button");
 loginButton.addEventListener("click", (e)=>{
     e.preventDefault();
-    // Comparo los datos del email del login con el del object User que creé en el registro, y comparo el password.
-    // El problema es que no se si puedo usar al user así, o tengo que importarlo desde  el localStorage
-    if (userEmailLogin.value === user.email && userPasswordLogin === user.password){
-        // Muestro mensaje de login correcto
+    // Traigo los datos del localStorage
+    const userRecovery = JSON.parse(localStorage.getItem("user"));
+    if (userEmailLogin.value === userRecovery.email && userPasswordLogin.value === userRecovery.password){
+        // Muestro el mensaje
         console.log("Login Correcto");
+    }else{
+        // Muestro el mensaje
+        console.log("Login Incorrecto");
     }
-    // Esto lo comento porque tengo dudas de como hacerlo. ¿Como selecciono el dato que quiero traer del localStorage, ej, traer solamente el email del user y el password? Porque entiendo que así traigo el user completo como un string en formato JSON.
-    // if (userEmailLogin.value === JSON.parse(localStorage.getItem("user")) && userPasswordLogin.value === JSON.parse(localStorage.getItem("user"))){
-    //     // Muestro el mensaje
-    //     console.log("Login Correcto");
-    // }
 })
