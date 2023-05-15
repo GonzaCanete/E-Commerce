da79f82e4d4e7dcf4d86bf0c693aaf74973fbe56
Gonzalo Canete <gonzalocb1997@gmail.com>
2023-05-15 12:09:48 -0300

    add

--- js/cart.js
+++ js/cart.js
@@ -3,3 +3,9 @@
 const cart = [];
 const cartTotal = 0;
 
+// Traigo el boton agregar al carrito 
+const cartButton = document.getElementById("add-cart");
+cartButton.addEventListener("click", () => {
+    // Traigo el producto seleccionado
+    console.log("holaaaa");
+})
